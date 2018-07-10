7a3a984563e4ae915d7443e2257fe97bdf35ffe5
oyvsik <leet.bab@gmail.com>
2018-07-10 21:24:13 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Map from './Map';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
+//var Map = require('./Map.js');
 
-class SimpleMap extends Component {
-    static defaultProps = {
-        center: {
-            lat: 50.95,
-            lng: 0.33
-        },
-        zoom: 6
-    };
-
+class App extends React.Component {
     render() {
         return (
-            // Important! Always set the container height explicitly
-            <div style={{ height: '50vh', width: '50%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo" }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text={'Kreyser Avrora'}
-                    />
-                </GoogleMapReact>
+            <div className='App'>
+                <Map />
             </div>
         );
     }
 }
 
-export default SimpleMap;
+
+export default App;
---
+++ src/Map.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Map extends Component {
+    static defaultProps = {
+        center: {
+            lat: 50.95,
+            lng: 0.33
+        },
+        zoom: 6
+    };
+
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '50vh', width: '50%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo" }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    <AnyReactComponent
+                        lat={59.955413}
+                        lng={30.337844}
+                        text={'Kreyser Avrora'}
+                    />
+                </GoogleMapReact>
+            </div>
+        );
+    }
+}
+
+//module.export = Map;
+export default Map;
