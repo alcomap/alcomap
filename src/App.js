bd7408e371658173763f6e36f107983d6426c54d
jkriland <john.kevin.riland@live.no>
2018-07-10 20:55:56 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './amlogo.png';
-import './App.css';
+import GoogleMapReact from 'google-map-react';
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
+class SimpleMap extends Component {
+    static defaultProps = {
+        center: {
+            lat: 50.95,
+            lng: 0.33
+        },
+        zoom: 6
+    };
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '50vh', width: '50%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo" }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    <AnyReactComponent
+                        lat={59.955413}
+                        lng={30.337844}
+                        text={'Kreyser Avrora'}
+                    />
+                </GoogleMapReact>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default SimpleMap;
