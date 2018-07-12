948ea79b9f1aafa66268bdb6d259c3f01d76fd7f
jkriland <john.kevin.riland@live.no>
2018-07-13 00:52:13 +0200

    google map sentralisering

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
+
+export const getCurrentLocation = () => {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
+    });
+};
+
 class Map extends Component {
     static defaultProps = {
         center: {
             lat: 50.95,
             lng: 0.33
         },
-        zoom: 13
+        zoom: 13,
+        centerAroundCurrentLocation: false
     };
+
+
+    /*
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.google !== this.props.google) {
+            this.loadMap();
+        }
+        if (prevState.currentLocation !== this.state.currentLocation) {
+            this.recenterMap();
+        }
+    }
+
+    recenterMap() {
+        const map = this.map;
+        const curr = this.state.currentLocation;
+
+        const google = this.props.google;
+        const maps = google.maps;
+
+        if (map) {
+            let center = new maps.LatLng(curr.lat, curr.lng)
+            map.panTo(center)
+        }
+    }
+
+    */
+
+    componentDidMount() {
+        return getCurrentLocation().then(position => {
+            if (position) {
+                this.setState({
+                    region: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        latitudeDelta: 0.003,
+                        longitudeDelta: 0.003,
+                    },
+                });
+            }
+        });
+    }
 
     render() {
         return (
