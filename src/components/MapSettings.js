1b3f0fc3bbdaf5b6068ef30ca2b2077fab952250
jkriland <john.kevin.riland@live.no>
2018-07-14 18:33:42 +0200

    sliders

--- src/components/MapSettings.js
+++ src/components/MapSettings.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
-import * as ReactBootstrap from 'react-bootstrap';
+import Tooltip from 'rc-tooltip';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css';
 
+
+
+const Handle = Slider.Handle;
+
+const handle = (props) => {
+    const { value, dragging, index, ...restProps } = props;
+    return (
+        <Tooltip
+            prefixCls="rc-slider-tooltip"
+            overlay={value}
+            visible={dragging}
+            placement="top"
+            key={index}
+        >
+            <Handle value={value} {...restProps} />
+        </Tooltip>
+    );
+};
+
+
+const slideStyle = { width: 350, margin: 50};
 
 class MapSettings extends Component {
-
     render() {
         return (
             <div className="mapDiv">
 
                 {/* Standard button */}
-                <ReactBootstrap.Button>Default</ReactBootstrap.Button>
+                {/* <ReactBootstrap.Button>Update</ReactBootstrap.Button> */}
 
 
+                <div style={slideStyle}>
+                    <h4 className="slideInfoText">Radius slider</h4>
+                    <Slider min={0} max={20} defaultValue={3} handle={handle} />
+                </div>
 
+                <div style={slideStyle}>
+                    <h4 className="slideInfoText">penislengde</h4>
+                    <Slider min={0} max={20} defaultValue={3} handle={handle} />
+                </div>
+
+                <div style={slideStyle}>
+                    <h4 className="slideInfoText">golifalidjnjasf</h4>
+                    <Slider min={0} max={20} defaultValue={3} handle={handle} />
+                </div>
+
+                <div style={slideStyle}>
+                    <h4 className="slideInfoText">derpdarp</h4>
+                    <Slider min={0} max={20} defaultValue={3} handle={handle} />
+                </div>
 
             </div>
 
         );
     }
+
 }
 
 //module.export = MapSettings;
